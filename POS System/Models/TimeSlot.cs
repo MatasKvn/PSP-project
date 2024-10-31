3fac06bda7f19583bfb4a298d4ed8650f44a0fcb
Nirtautas Šadauskas <nirtautas.sadauskas@gmail.com>
2024-10-31 12:29:22 +0200

    Added TimeSlot model.

---
+++ POS System/Models/TimeSlot.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace POS_System.Models
+{
+    [Table("TimeSlot")]
+    public class TimeSlot
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public TimeSlot(int id, DateTime startDate, bool isAvailable = true)
+        {
+            Id = id;
+            StartDate = startDate;
+            IsAvailable = isAvailable;
+        }
+    }
+}
