0ff88b7679c651de30de04a55cac145af442c46c
Nirtautas Šadauskas <nirtautas.sadauskas@gmail.com>
2024-10-31 12:39:05 +0200

    Added cart model.

---
+++ POS System/Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace POS_System.Models
+{
+    [Table("Cart")]
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; private set; }
+
+        public Cart(int id)
+        {
+            this.Id = id;
+            this.DateCreated = DateTime.Now;
+        }
+    }
+}
