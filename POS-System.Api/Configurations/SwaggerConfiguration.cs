a9c79d4f8844ae385aac9947b7f21716b33fe757
pkondratas <paulius.kondratas@gmail.com>
2024-11-26 22:59:33 +0200

    Add authorization header field in Swagger

--- POS-System.Api/Configurations/SwaggerConfiguration.cs
+++ POS-System.Api/Configurations/SwaggerConfiguration.cs
@@ -8,10 +8,36 @@ public static class SwaggerConfiguration
     {
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen(options =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo { Title = "POS-System API", Version = "v1" });
-            options.UseDateOnlyTimeOnlyStringConverters();
-        });
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                options.UseDateOnlyTimeOnlyStringConverters();
+                // Add security definition
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' followed by a space and your JWT token."
+                });
+
+                // Add security requirement
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
 
         return builder;
     }
