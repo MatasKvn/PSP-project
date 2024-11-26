d6b28c0c7088c8b351cf37e991ccc6832174d85f
Vilius Paskonis <vilius.paskonis@gmail.com>
2024-11-26 17:43:32 +0200

    Implement ProductModification CRUD

--- POS-System.Business/AutoMapper/MappingProfile.cs
+++ POS-System.Business/AutoMapper/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using POS_System.Business.Dtos;
+using POS_System.Business.Dtos.ProductModificationDtos;
 using POS_System.Domain.Entities;
 
 namespace POS_System.Business.AutoMapper
@@ -10,6 +11,12 @@ namespace POS_System.Business.AutoMapper
         {
             CreateMap<Tax, TaxDto>();
             CreateMap<TaxDto, Tax>();
+
+            // ProductModification
+            CreateMap<ProductModification, GetProductModificationDto>();
+            CreateMap<GetProductModificationDto, ProductModification>();
+            CreateMap<ProductModification, CreateProductModificationDto>();
+            CreateMap<CreateProductModificationDto, ProductModification>();
         }
     }
 }
