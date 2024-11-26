ec6fb661a4de08809f4cbae70177a6265a8408bc
Vilius Paskonis <vilius.paskonis@gmail.com>
2024-11-26 17:50:45 +0200

    Remove Data Annotations from TimeSlotDtos

--- POS-System.Business/Dtos/TimeSlotDtos/CreateTimeSlotDto.cs
+++ POS-System.Business/Dtos/TimeSlotDtos/CreateTimeSlotDto.cs
@@ -1,14 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace POS_System.Business.Dtos.TimeSlotDtos
+﻿namespace POS_System.Business.Dtos.TimeSlotDtos
 {
     public record CreateTimeSlotDto
     {
-        //Foreign keys
-        [ForeignKey("Employee")]
         public int EmployeeVersionId { get; set; }
-
-        //Fields
         public required DateTime StartTime { get; set; }
         public required bool IsAvailable { get; set; }
     }
--- POS-System.Business/Dtos/TimeSlotDtos/GetTimeSlotDto.cs
+++ POS-System.Business/Dtos/TimeSlotDtos/GetTimeSlotDto.cs
@@ -1,18 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace POS_System.Business.Dtos.TimeSlotDtos
+﻿namespace POS_System.Business.Dtos.TimeSlotDtos
 {
     public record GetTimeSlotDto
     {
-        [Key]
         public int Id { get; set; }
-
-        //Foreign keys
-        [ForeignKey("Employee")]
         public int EmployeeVersionId { get; set; }
-
-        //Fields
         public required DateTime StartTime { get; set; }
         public required bool IsAvailable { get; set; }
     }
