117734936a97a4b5a3e23252ce51a2e1a41eb68a
Vilius Paskonis <vilius.paskonis@gmail.com>
2024-11-26 17:47:52 +0200

    Removed Data Annotations ins ProductDtos

--- POS-System.Business/Dtos/ProductDtos/CreateProductDto.cs
+++ POS-System.Business/Dtos/ProductDtos/CreateProductDto.cs
@@ -1,10 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace POS_System.Business.Dtos.ProductDtos
+﻿namespace POS_System.Business.Dtos.ProductDtos
 {
     public record CreateProductDto
     {
-        [MaxLength(40)]
         public required string Name { get; set; }
         public required string Description { get; set; }
         public required int Price { get; set; }
