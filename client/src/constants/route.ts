b88e17df7e2ada198b097f44056b3cd7e5ff8e64
MatasKvn <matas.kvainauskas@protonmail.com>
2024-12-06 14:06:43 +0200

    Add Taxes page link to navbar

--- client/src/constants/route.ts
+++ client/src/constants/route.ts
@@ -7,5 +7,6 @@ export const GetPageUrl = {
     login: '/login',
     carts: (pageNumber: number) => `/dashboard/carts/${pageNumber}`,
     cart: (cartId: number, pageNumber: number) => `/dashboard/cart/${cartId}?pageNumber=${pageNumber}`,
-    products: (pageNumber: number) => `/dashboard/products/${pageNumber}`
+    products: (pageNumber: number) => `/dashboard/products/${pageNumber}`,
+    taxes: (pageNumber: number) => `/dashboard/taxes/${pageNumber}`
 } as const
