574307ab27736fc9f40f02a422854f768f550b27
MatasKvn <matas.kvainauskas@protonmail.com>
2024-12-06 14:05:40 +0200

    Create tax api functions

--- client/src/api/tax.api.ts
+++ client/src/api/tax.api.ts
@@ -1,4 +1,4 @@
-import { FetchResponse } from '@/types/fetch'
+import { FetchResponse, PagedResponse } from '@/types/fetch'
 import { Tax } from '@/types/models'
 
 const tax: Tax = {
@@ -10,6 +10,41 @@ const tax: Tax = {
 }
 
 export default class TaxApi {
+    static async getAllTaxes(pageNumber: number): Promise<FetchResponse<PagedResponse<Tax>>> {
+        return Promise.resolve({
+            result: {
+                pageNum: pageNumber,
+                pageSize: 35,
+                totalCount: 1,
+                results: [tax]
+            }
+        })
+    }
+
+    static async createTax(taxRequest: CreateTaxRequest): Promise<FetchResponse<Tax>> {
+        return Promise.resolve({
+            result: {
+                ...taxRequest,
+                id: 1,
+                dateModified: new Date()
+            }
+        })
+    }
+
+    static async updateTax(taxRequest: UpdateTaxRequest): Promise<FetchResponse<Tax>> {
+        return Promise.resolve({
+            result: {
+                ...taxRequest,
+                dateModified: new Date()
+            }
+        })
+    }
+
+    static async deleteTax(id: number): Promise<FetchResponse<any>> {
+        if (id !== 1) return Promise.resolve({ error: 'Tax not found' })
+        return Promise.resolve({ result: tax })
+    }
+
     static async getTaxesByProductId(productId: number): Promise<FetchResponse<Tax[]>> {
         return Promise.resolve({
             result: [tax]
@@ -21,3 +56,6 @@ export default class TaxApi {
         })
     }
 }
+
+type CreateTaxRequest = Omit<Tax, 'id' | 'dateModified'>
+type UpdateTaxRequest = Omit<Tax, 'dateModified'>
